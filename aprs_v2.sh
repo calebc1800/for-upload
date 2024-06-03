eb80b93fc4c7b539610585d484562b2fb68864a3
calebc1800 <40322077+calebc1800@users.noreply.github.com>
2024-06-03 17:55:56 -0600

    new direwolf script

--- aprs_v2.sh
+++ aprs_v2.sh
@@ -2,9 +2,34 @@
 # script to convert signal into packets using direwolf and rx_fm
 
 # Variables
-FREQUENCY=434.9e6
+FREQUENCY=434.905M # Previous Value: 434.9e6
 SAMPLE=48000
-CONFIG=/home/cubesat-gs/CubeSatSim/direwolf.conf
+CONFIG='/home/cubesat-gs/CubeSatSim-1.3.2/groundstation/direwolf/direwolf.conf'
+
+# Start loopback interface and kill all possible conflicting programs
+sudo modprobe snd-aloop
+
+sudo systemctl stop openwebrx
+
+sudo systemctl stop rtl_tcp
+
+pkill -o chromium &>/dev/null
+
+sudo killall -9 rtl_fm &>/dev/null
+
+sudo killall -9 direwolf &>/dev/null
+
+sudo killall -9 aplay &>/dev/null
+
+sudo killall -9 qsstv &>/dev/null
+
+sudo killall -9 rtl_tcp &>/dev/null
+
+sudo killall -9 java &>/dev/null
+
+sudo killall -9 CubicSDR &>/dev/null
+
+sudo killall -9 zenity &>/dev/null
 
 # HackRF
 SoapySDRUtil --probe="driver=hackrf" > /dev/null 2>&1
@@ -19,7 +44,7 @@ echo "$value" > /dev/null
 set -- $value
 
 # Start rx_fm
-rx_fm -M fm -f $frequency -s 48k | tee >(aplay -D hw:${2:0:1},0,0 -r 48000 -t raw -f S16_LE -c 1) | aplay -D hw:0,0 -r 48000 -t raw -f S16_LE -c 1
+rx_fm -M fm -f $FREQUENCY -s $SAMPLE -l 30 -w 48000 -vvvv -o 1,4 | tee >(aplay -D hw:${2:0:1},0,0 -r 48000 -t raw -f S16_LE -c 1) | aplay -D hw:0,0 -r 48000 -t raw -f S16_LE -c 1
 # The above line uses the tee command to send the output of rtl_fm to two different aplay commands. 
 # The first aplay command plays the audio on a specific hardware device (determined by ${2:0:1}), and 
 # the second aplay command plays the audio on the default hardware device (hw:0,0). 
---
+++ direwolf-conf/xdirewolf.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# Script to start x direwolf instances each with their own configuration file and udp port source
+
+# Variables
+WORKING_DIR=$(pwd)
+MAX_INSTANCES=10
+SAMPLE=48000
+INIT_AGWPORT=8000
+INIT_KISSPORT=8001
+INIT_UDP_PORT=1800
+
+# Read amount of direwolf instances from command line
+NUM_INSTANCES=$1
+
+# Check if the number of instances is a number and is less than or equal to the maximum allowed
+if ! [[ $NUM_INSTANCES =~ ^[0-9]+$ ]] || [ $NUM_INSTANCES -gt $MAX_INSTANCES ]; then
+    echo "Usage: $0 <number of instances>"
+    echo "The maximum number of instances is $MAX_INSTANCES"
+    exit 1
+fi
+
+# Start loopback interface and kill all possible conflicting programs
+sudo modprobe snd-aloop
+sudo systemctl stop openwebrx &>/dev/null
+sudo systemctl stop rtl_tcp &>/dev/null
+pkill -o chromium &>/dev/null
+sudo killall -9 rtl_fm &>/dev/null
+sudo killall -9 direwolf &>/dev/null
+sudo killall -9 aplay &>/dev/null
+sudo killall -9 qsstv &>/dev/null
+sudo killall -9 rtl_tcp &>/dev/null
+sudo killall -9 java &>/dev/null
+sudo killall -9 CubicSDR &>/dev/null
+sudo killall -9 zenity &>/dev/null
+
+# Make configuration directory
+mkdir -p $WORKING_DIR/dire-configs
+
+# Print information
+echo "Starting $NUM_INSTANCES direwolf instances"
+echo "Sample rate: $SAMPLE"
+
+# Start each direwolf instance
+for i in $(seq 0 ($NUM_INSTANCES - 1)); do
+    # Variables
+    AGWPORT=$((INIT_AGWPORT + (i * 1000)))
+    KISSPORT=$((INIT_KISSPORT + (i * 1000)))
+    UDP_PORT=$((INIT_UDP_PORT + (i * 100)))
+
+    # Create configuration file
+    cat > $WORKING_DIR/dire-configs/direwolf$i.conf << EOF
+    AGWPORT $AGWPORT
+    KISSPORT $KISSPORT
+    ADEVICE UDP:$UDP_PORT default
+EOF
+
+    # Start direwolf in seperate terminal tabs with configuration file and sample rate print information after each instance
+    gnome-terminal --tab --title="Direwolf $i" -- bash -c "direwolf -c $WORKING_DIR/dire-configs/direwolf$i.conf -r $SAMPLE -t 0"
+    sleep 1
+    echo "Direwolf $i started with AGWPORT: $AGWPORT, KISSPORT: $KISSPORT, and UDP_PORT: $UDP_PORT"
+done
